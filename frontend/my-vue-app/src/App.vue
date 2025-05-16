f7ab79a6a87cc9d0bb89a94bbb4ec7f7dafea193
kasra khaksar <ksra13khaksar@gmail.com>
2025-05-16 13:34:04 +0330

    first commit

---
+++ frontend/my-vue-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+
+    <UserCard/>
+
+  </div>
+</template>
+
+
+
+<script>
+import UserCard from './components/UserCard.vue'
+
+export default {
+    name: 'App',
+    components: {
+      UserCard
+    }}
+</script>
+
+<style scoped>
+.container {
+  padding: 20px;
+}
+</style>
---
+++ frontend/my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+      <h2>لیست علاقه‌مندی‌ها:</h2>
+      <ul>
+        <li v-for="(item) in favorites" :key="item">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+
+    <UserCard name="رضا" :age="27" job="مدیر محصول" />
+  </template>
+  
+
+  <script>
+
+    import UserCard from './UserCard.vue'
+
+    export default {
+    components: {
+        UserCard
+    },
+
+    
+    data() {
+        return {
+        favorites: ['موسیقی', 'برنامه‌نویسی', 'کتاب', 'ورزش']
+        }
+    }
+    }
+
+
+  </script>
+
+
+
+  
+
+
+
+  <style scoped>
+  .container {
+    padding: 20px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    background: #eee;
+    margin-bottom: 8px;
+    padding: 10px;
+    border-radius: 5px;
+  }
+  </style>
+  
