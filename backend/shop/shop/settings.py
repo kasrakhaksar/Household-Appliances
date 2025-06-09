26812c36c6631c3f77f21dd350f8ec7acf3aec56
kasra khaksar <ksra13khaksar@gmail.com>
2025-06-09 21:32:11 +0330

    backend : add redis and mysql database

--- backend/shop/shop/settings.py
+++ backend/shop/shop/settings.py
@@ -76,10 +76,29 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'env',
+        'USER': 'root',
+        'PASSWORD': 'Kasrakhaksar1313',
+        'HOST': 'localhost',
+        'PORT': '3306',  
+    }
+}
+
+
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',  
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
 
