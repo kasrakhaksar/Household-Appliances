3fcfbd2aad0ca19ca6709369c808c2a6d93f47c3
kasrakhaksar <ksra13khaksar@gmail.com>
2025-11-23 16:27:37 +0330

    change docker files

--- backend/shop/shop/settings.py
+++ backend/shop/shop/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': "django.db.backends.mysql",
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
